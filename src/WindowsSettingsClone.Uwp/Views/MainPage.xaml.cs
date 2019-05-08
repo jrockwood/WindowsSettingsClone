1eb6d32d34750bf955cfb33b61754104264c7010
Justin Rockwood <justinrockwood@hotmail.com>
2019-05-07 23:17:33 -0700

    Initial commit

---
+++ src/WindowsSettingsClone.Uwp/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone
+{
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
