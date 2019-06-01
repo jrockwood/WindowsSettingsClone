ad3bd6079dc49fc6299015e4a48d6e44b0d5aa71
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-01 16:08:27 -0700

    Add ICommand<T> and RelayCommand

---
+++ src/WindowsSettingsClone.Uwp.ViewModels/Utility/RelayCommand.cs
@@ -0,0 +1,70 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="RelayCommand.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.ViewModels.Utility
+{
+    using System;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Implements an <see cref="ICommand"/> interface by calling delegates specified in the constructor.
+    /// </summary>
+    internal class RelayCommand<T> : ICommand<T>
+    {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private bool _enabled;
+        private readonly Action<T> _executeAction;
+
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public RelayCommand(Action<T> executeAction, bool enabled = true)
+        {
+            _executeAction = Param.VerifyNotNull(executeAction, nameof(executeAction));
+            _enabled = enabled;
+        }
+
+        //// ===========================================================================================================
+        //// Events
+        //// ===========================================================================================================
+
+        public event EventHandler CanExecuteChanged;
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                if (_enabled != value)
+                {
+                    _enabled = value;
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        bool ICommand.CanExecute(object parameter) => _enabled;
+
+        void ICommand.Execute(object parameter) => _executeAction((T)parameter);
+
+        public bool CanExecute(T parameter) => _enabled;
+
+        public void Execute(T parameter) => _executeAction(parameter);
+    }
+}
