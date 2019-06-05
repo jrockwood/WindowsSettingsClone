9daadfb698340cd8a26ea25cb0bf3990788b569d
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-04 17:57:37 -0700

    Add IPlatformCapabilityService

---
+++ src/WindowsSettingsClone.Uwp.ViewModels/ViewServices/IPlatformCapabilityService.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="IPlatformCapabilityService.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.ViewModels.ViewServices
+{
+    /// <summary>
+    /// Interface for querying the underlying platform capabilities.
+    /// </summary>
+    public interface IPlatformCapabilityService
+    {
+        bool IsRevealBrushSupported { get; }
+    }
+}
