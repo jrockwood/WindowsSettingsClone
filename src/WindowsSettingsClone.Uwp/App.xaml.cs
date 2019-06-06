62ed61e84a7d1f261d0d9f7d5763303fa1b047a7
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-05 19:42:59 -0700

    Create RootPage with a title bar and frame

--- src/WindowsSettingsClone.Uwp/App.xaml.cs
+++ src/WindowsSettingsClone.Uwp/App.xaml.cs
@@ -8,8 +8,8 @@
 namespace WindowsSettingsClone.Uwp
 {
     using System;
-    using ViewModels;
     using ViewModels.ViewServices;
+    using Views;
     using ViewServices;
     using Windows.ApplicationModel;
     using Windows.ApplicationModel.Activation;
@@ -17,7 +17,6 @@ namespace WindowsSettingsClone.Uwp
     using Windows.UI.ViewManagement;
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
-    using Windows.UI.Xaml.Navigation;
 
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
@@ -29,8 +28,8 @@ namespace WindowsSettingsClone.Uwp
         //// ===========================================================================================================
 
         /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// Initializes the singleton application object. This is the first line of authored code executed, and as such
+        /// is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
@@ -44,15 +43,16 @@ namespace WindowsSettingsClone.Uwp
 
         public static new App Current => (App)Application.Current;
 
-        public INavigationViewService NavigationService { get; private set; }
+        public INavigationViewService NavigationService =>
+            ((Window.Current.Content as Frame)?.Content as RootPage).NavigationService;
 
         //// ===========================================================================================================
         //// Methods
         //// ===========================================================================================================
 
         /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
+        /// Invoked when the application is launched normally by the end user. Other entry points will be used such as
+        /// when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
@@ -66,29 +66,23 @@ namespace WindowsSettingsClone.Uwp
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
-                rootFrame.NavigationFailed += OnNavigationFailed;
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
                 }
 
-                // Initialize the navigation service
-                NavigationService = new NavigationService(rootFrame);
-
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
-            if (e.PrelaunchActivated == false)
+            if (!e.PrelaunchActivated)
             {
                 if (rootFrame.Content == null)
                 {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    NavigationService.NavigateTo(typeof(HomePageViewModel), e.Arguments);
+                    rootFrame.SourcePageType = typeof(RootPage);
                 }
+
                 // Ensure the current window is active
                 Window.Current.Activate();
             }
@@ -119,17 +113,8 @@ namespace WindowsSettingsClone.Uwp
         }
 
         /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e) =>
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
+        /// Invoked when application execution is being suspended. Application state is saved without knowing whether the
+        /// application will be terminated or resumed with the contents of memory still intact.
         /// </summary>
         /// <param name="sender">The source of the suspend request.</param>
         /// <param name="e">Details about the suspend request.</param>
