6a95f5933e5d86e285d5eedeecfc18c109922a44
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-07 23:33:30 -0700

    Add a SettingPageHost to the SettingsGroupPage

---
+++ src/WindowsSettingsClone.Uwp/Views/SettingPageHost.xaml.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="SettingPageHost.xaml.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.Views
+{
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class SettingPageHost : UserControl
+    {
+        public SettingPageHost() => InitializeComponent();
+    }
+}
