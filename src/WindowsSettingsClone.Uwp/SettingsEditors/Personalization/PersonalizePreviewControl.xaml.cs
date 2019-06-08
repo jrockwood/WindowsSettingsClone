d22c61e4d2ea22cef7a688df3f02bd281676141f
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-08 13:57:11 -0700

    Add placeholder editor for the Background settings

---
+++ src/WindowsSettingsClone.Uwp/SettingsEditors/Personalization/PersonalizePreviewControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="PersonalizePreviewControl.xaml.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.SettingsEditors.Personalization
+{
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class PersonalizePreviewControl : UserControl
+    {
+        public PersonalizePreviewControl() => InitializeComponent();
+    }
+}
