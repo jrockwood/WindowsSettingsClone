1d64d2a257de071fff55183e9b6e9f95d08f2580
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-13 18:45:24 -0700

    Add NamedValue<T> to represent a setting that has a value and display name

---
+++ src/WindowsSettingsClone.Uwp.ViewModels/NamedValue.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="NamedValue.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.ViewModels
+{
+    public class NamedValue<T>
+    {
+        public NamedValue(string displayName, T value)
+        {
+            DisplayName = displayName;
+            Value = value;
+        }
+
+        public string DisplayName { get; }
+        public T Value { get; }
+    }
+}
