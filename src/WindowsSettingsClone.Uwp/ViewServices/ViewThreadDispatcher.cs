5e481596092931e5a9cf3934055a69131a2d128c
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-16 10:13:38 -0700

    Add IThreadDispatcher, its implementation, and unit tests

---
+++ src/WindowsSettingsClone.Uwp/ViewServices/ViewThreadDispatcher.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ViewThreadDispatcher.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.ViewServices
+{
+    using System;
+    using System.Threading.Tasks;
+    using ViewModels.ViewServices;
+    using Windows.ApplicationModel.Core;
+    using Windows.UI.Core;
+
+    /// <summary>
+    /// Default implementation of <see cref="IThreadDispatcher"/> that uses the application's main <see cref="CoreDispatcher"/>.
+    /// </summary>
+    internal class ViewThreadDispatcher : ThreadDispatcher
+    {
+        public ViewThreadDispatcher()
+            : base(RunOnCoreWindowDispatcher)
+        {
+        }
+
+        public static Task RunOnCoreWindowDispatcher(Action action) => CoreApplication.MainView.CoreWindow.Dispatcher
+            .RunAsync(CoreDispatcherPriority.Normal, () => action())
+            .AsTask();
+    }
+}
