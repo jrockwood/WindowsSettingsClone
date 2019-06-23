d84ad55c44d6a7cf7d59b9b9e0d35543184b65b4
Justin Rockwood <justinrockwood@hotmail.com>
2019-06-22 17:06:14 -0700

    Add the non-administrator DesktopServices project

---
+++ src/WindowsSettingsClone.Uwp.DesktopServices/Program.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.Uwp.DesktopServices
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
