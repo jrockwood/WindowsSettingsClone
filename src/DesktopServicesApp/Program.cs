e67f78b18cfac0821f8bebbee5478d25c31d318a
Justin Rockwood <justinrockwood@hotmail.com>
2019-07-06 16:41:10 -0400

    Extract methods in Program.cs

--- src/DesktopServicesApp/Program.cs
+++ src/DesktopServicesApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 // <copyright file="Program.cs" company="Justin Rockwood">
 //   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
 //   LICENSE.txt in the project root for license information.
@@ -14,15 +14,42 @@ namespace WindowsSettingsClone.DesktopServicesApp
 
     internal class Program
     {
-        private static void Main(string[] args)
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private readonly ILogger _logger;
+
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public Program()
+        {
+            _logger = CreateLogger();
+        }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        private static ILogger CreateLogger()
         {
 #if DEBUG
             const LogLevel minimumLogLevel = LogLevel.Debug;
 #else
             const LogLevel minimumLogLevel = LogLevel.Warning;
 #endif
-            ILogger logger = new ConsoleLogger(minimumLogLevel);
+            var consoleLogger = new ConsoleLogger(minimumLogLevel);
+            var fileLogger = new FileLogger(
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "debug.log"),
+                LogLevel.Debug);
+            ILogger logger = new AggregateLogger(consoleLogger, fileLogger);
+            return logger;
+        }
 
+        private static void Main(string[] args)
+        {
             // To debug this app, you'll need to have it started in console mode. Uncomment the lines below and then
             // right-click on the project file to get to project settings. Select the Application tab and change the
             // Output Type from Windows Application to Console Application. A "Windows Application" is simply a headless
@@ -32,17 +59,23 @@ namespace WindowsSettingsClone.DesktopServicesApp
             //Console.WriteLine("Set your breakpoint in DesktopServices and then press Enter to continue.");
             //Console.ReadLine();
 
-            var bridge = new UwpCommunicationBridge(logger);
+            var program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            var bridge = new UwpCommunicationBridge(_logger);
 
             // We can't have an async entry point, so do this on the thread pool.
             bool success = Task.Run(async () => await bridge.InitializeAsync()).GetAwaiter().GetResult();
             if (!success)
             {
-                logger.LogError("Cannot establish app service connection");
+                _logger.LogError("Cannot establish app service connection");
                 return;
             }
 
-            logger.LogSuccess("Established app service connection");
+            _logger.LogSuccess("Established app service connection");
 
             // Let the app service connection handlers respond to events. If this Win32 app had a Window, this would be a
             // message loop. The app ends when the app service connection to the UWP app is closed and our
