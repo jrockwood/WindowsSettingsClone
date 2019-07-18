cec512188da390d68359d9239fa5059a1cd4540a
Justin Rockwood <justinrockwood@hotmail.com>
2019-07-17 17:17:05 -0700

    Add FakeRegistry and unit tests

---
+++ src/SharedWin32Tests/Fakes/FakeRegistry.cs
@@ -0,0 +1,186 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="FakeRegistry.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.SharedWin32Tests.Fakes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.Win32;
+    using SharedWin32.CommandExecutors.Registry;
+
+    /// <summary>
+    /// Implementation of a fake registry.
+    /// </summary>
+    public class FakeRegistry : IWin32Registry
+    {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private readonly Dictionary<string, Dictionary<string, object>> _keys =
+            new Dictionary<string, Dictionary<string, object>>(StringComparer.OrdinalIgnoreCase);
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public IWin32RegistryKey OpenBaseKey(RegistryHive hKey, RegistryView view)
+        {
+            if (view != RegistryView.Registry64)
+            {
+                throw new ArgumentException("View type not supported", nameof(view));
+            }
+
+            return CreateSubKey(HiveToWin32Name(hKey), false);
+        }
+
+        private static string HiveToWin32Name(RegistryHive hive)
+        {
+            switch (hive)
+            {
+                case RegistryHive.ClassesRoot:
+                    return "HKEY_CLASSES_ROOT";
+
+                case RegistryHive.CurrentUser:
+                    return "HKEY_CURRENT_USER";
+
+                case RegistryHive.LocalMachine:
+                    return "HKEY_LOCAL_MACHINE";
+
+                case RegistryHive.Users:
+                    return "HKEY_USERS";
+
+                case RegistryHive.PerformanceData:
+                case RegistryHive.CurrentConfig:
+                case RegistryHive.DynData:
+                    throw new NotSupportedException();
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(hive), hive, null);
+            }
+        }
+
+        private IWin32RegistryKey OpenSubKey(string fullPath, bool writable)
+        {
+            return _keys.ContainsKey(fullPath) ? new Key(this, fullPath, writable) : null;
+        }
+
+        private IWin32RegistryKey CreateSubKey(string fullPath, bool writable)
+        {
+            IWin32RegistryKey subKey = OpenSubKey(fullPath, writable);
+            if (subKey == null)
+            {
+                subKey = new Key(this, fullPath, writable);
+                _keys.Add(fullPath, null);
+
+                // Also add any nested paths
+                string[] nestedPathSegments = fullPath.Split('\\');
+                while (nestedPathSegments.Length > 1)
+                {
+                    string path = string.Join("\\", nestedPathSegments.Take(nestedPathSegments.Length - 1));
+                    if (_keys.ContainsKey(path))
+                    {
+                        break;
+                    }
+
+                    _keys.Add(path, null);
+                    nestedPathSegments = path.Split('\\');
+                }
+            }
+
+            return subKey;
+        }
+
+        //// ===========================================================================================================
+        //// Classes
+        //// ===========================================================================================================
+
+        private sealed class Key : IWin32RegistryKey
+        {
+            private readonly FakeRegistry _registry;
+            private bool _isDisposed;
+
+            public Key(FakeRegistry registry, string fullPath, bool writable)
+            {
+                _registry = registry;
+                FullPath = fullPath;
+                Writable = writable;
+            }
+
+            private string FullPath { get; }
+            private bool Writable { get; }
+
+            public void Dispose()
+            {
+                _isDisposed = true;
+            }
+
+            public IWin32RegistryKey OpenSubKey(string name, bool writable)
+            {
+                ThrowIfDisposedOnNonBaseKey();
+                return _registry.OpenSubKey($"{FullPath}\\{name}", writable);
+            }
+
+            public IWin32RegistryKey CreateSubKey(string name, bool writable)
+            {
+                ThrowIfDisposedOnNonBaseKey();
+                return _registry.CreateSubKey($"{FullPath}\\{name}", writable);
+            }
+
+            public object GetValue(string name)
+            {
+                ThrowIfDisposedOnNonBaseKey();
+                return GetValue(name, null);
+            }
+
+            public object GetValue(string name, object defaultValue)
+            {
+                ThrowIfDisposedOnNonBaseKey();
+
+                Dictionary<string, object> values = _registry._keys[FullPath];
+                if (values == null || !values.TryGetValue(name, out object value))
+                {
+                    value = defaultValue;
+                }
+
+                return value;
+            }
+
+            public void SetValue(string name, object value, RegistryValueKind valueKind)
+            {
+                ThrowIfDisposedOnNonBaseKey();
+
+                if (!Writable)
+                {
+                    throw new UnauthorizedAccessException("Cannot write to the registry key.");
+                }
+
+                CreateSubKey(name, false);
+
+                _registry._keys.TryGetValue(FullPath, out Dictionary<string, object> values);
+
+                values = values ?? new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+                _registry._keys[FullPath] = values;
+
+                values[name] = value;
+            }
+
+            /// <summary>
+            /// Throws an <see cref="ObjectDisposedException"/> if necessary. Note that operations are still allowed on
+            /// root keys even when disposed.
+            /// </summary>
+            private void ThrowIfDisposedOnNonBaseKey()
+            {
+                if (_isDisposed && FullPath.Contains("\\"))
+                {
+                    throw new ObjectDisposedException(FullPath, "Cannot access a closed registry key.");
+                }
+            }
+        }
+    }
+}
---
+++ src/SharedWin32Tests/Fakes/FakeRegistryBehaviorEquivalenceTests.cs
@@ -0,0 +1,126 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="FakeRegistryBehaviorEquivalenceTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.SharedWin32Tests.Fakes
+{
+    using System;
+    using FluentAssertions;
+    using Microsoft.Win32;
+    using NUnit.Framework;
+    using SharedWin32.CommandExecutors.Registry;
+
+    public class FakeRegistryBehaviorEquivalenceTests
+    {
+        private const string TestPath = @"__RockwoodTestTemp__";
+
+        [Test]
+        [Category("SkipWhenLiveUnitTesting")]
+        public void SetValue_throws_the_same_exception_as_the_real_registry()
+        {
+            Exception expectedException = null;
+
+            using (var hkcu = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+            using (RegistryKey testKey = hkcu.CreateSubKey(TestPath, writable: false))
+            {
+                try
+                {
+                    testKey.SetValue("TestValue", 123, RegistryValueKind.DWord);
+                }
+                catch (Exception e)
+                {
+                    expectedException = e;
+                }
+                finally
+                {
+                    hkcu.DeleteSubKey(TestPath, throwOnMissingSubKey: false);
+                }
+
+                expectedException.Should().NotBeNull();
+            }
+
+            var fakeRegistry = new FakeRegistry();
+            using (IWin32RegistryKey hkcu = fakeRegistry.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+            using (IWin32RegistryKey testKey = hkcu.CreateSubKey(TestPath, writable: false))
+            {
+                // ReSharper disable once AccessToDisposedClosure
+                Action action = () => testKey.SetValue("TestValue", 123, RegistryValueKind.DWord);
+                action.Should()
+                    .Throw<Exception>()
+                    .And.Should()
+                    .BeEquivalentTo(
+                        expectedException,
+                        options => options.Excluding(e => e.TargetSite)
+                            .Excluding(e => e.StackTrace)
+                            .Excluding(e => e.Source)
+                            .Excluding(info => info.SelectedMemberInfo.Name == "IPForWatsonBuckets"));
+            }
+        }
+
+        [Test]
+        [Category("SkipWhenLiveUnitTesting")]
+        public void Closing_a_base_key_does_not_throw_ObjectDisposedException()
+        {
+            var realKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            realKey.Dispose();
+            Action action = () => realKey.CreateSubKey(TestPath);
+            action.Should().NotThrow();
+
+            var fakeRegistry = new FakeRegistry();
+            IWin32RegistryKey fakeKey = fakeRegistry.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            fakeKey.Dispose();
+            action = () => fakeKey.CreateSubKey(TestPath);
+            action.Should().NotThrow();
+        }
+
+        [Test]
+        [Category("SkipWhenLiveUnitTesting")]
+        public void An_operation_on_a_disposed_key_throws_the_same_exception_as_the_real_registry()
+        {
+            Exception expectedException = null;
+
+            using (var hkcu = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+            {
+                RegistryKey testKey = hkcu.CreateSubKey(TestPath, writable: false);
+                testKey.Dispose();
+
+                try
+                {
+                    testKey.GetValue("TestValue");
+                }
+                catch (Exception e)
+                {
+                    expectedException = e;
+                }
+                finally
+                {
+                    hkcu.DeleteSubKey(TestPath, throwOnMissingSubKey: false);
+                }
+
+                expectedException.Should().NotBeNull();
+            }
+
+            var fakeRegistry = new FakeRegistry();
+            using (IWin32RegistryKey hkcu = fakeRegistry.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+            {
+                IWin32RegistryKey testKey = hkcu.CreateSubKey(TestPath, writable: false);
+                testKey.Dispose();
+
+                // ReSharper disable once AccessToDisposedClosure
+                Action action = () => testKey.SetValue("TestValue", 123, RegistryValueKind.DWord);
+                action.Should()
+                    .Throw<Exception>()
+                    .And.Should()
+                    .BeEquivalentTo(
+                        expectedException,
+                        options => options.Excluding(e => e.TargetSite)
+                            .Excluding(e => e.StackTrace)
+                            .Excluding(e => e.Source)
+                            .Excluding(info => info.SelectedMemberInfo.Name == "IPForWatsonBuckets"));
+            }
+        }
+    }
+}
