6a367f14f0eecd618a16e4b851e3d7c358d0de26
Justin Rockwood <justinrockwood@hotmail.com>
2019-07-22 16:29:35 -0700

    Create ConsoleStreamServer and associated unit tests

---
+++ src/ElevatedDesktopServicesApp/ConsoleStreamServer.cs
@@ -0,0 +1,106 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleStreamServer.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.ElevatedDesktopServicesApp
+{
+    using ServiceContracts.CommandBridge;
+    using ServiceContracts.Logging;
+    using Shared.CommandBridge;
+    using Shared.Diagnostics;
+    using Shared.Logging;
+    using SharedWin32.CommandExecutors;
+    using SharedWin32.CommandExecutors.Registry;
+
+    /// <summary>
+    /// Function definition for reading a line from the underlying stream. Useful for unit tests.
+    /// </summary>
+    public delegate string ReadLineFunc();
+
+    /// <summary>
+    /// Function definition for writing a line to a stream. Useful for unit tests.
+    /// </summary>
+    /// <param name="value"></param>
+    public delegate void WriteLineAction(string value);
+
+    /// <summary>
+    /// Handles incoming requests that come across a redirected standard input stream and sends responses back on the
+    /// standard output stream. Errors are sent across the standard output stream, but a terminating exception is sent on
+    /// the standard error stream.
+    /// </summary>
+    public sealed class ConsoleStreamServer
+    {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private readonly ILogger _logger;
+
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public ConsoleStreamServer(ILogger logger)
+        {
+            _logger = Param.VerifyNotNull(logger, nameof(logger));
+        }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        /// <summary>
+        /// Starts the server by waiting for requests (via the <paramref name="readLineFunc"/>) and then sending
+        /// responses via <paramref name="writeLineAction"/>.
+        /// </summary>
+        /// <param name="readLineFunc">
+        /// The function to call to wait for an incoming command, serialized on a single line.
+        /// </param>
+        /// <param name="writeLineAction">
+        /// The action to call when a response is sent back to the client, serialized on a singe line.
+        /// </param>
+        /// <param name="registry">A <see cref="IWin32Registry"/> to use for executing registry commands.</param>
+        public void Start(ReadLineFunc readLineFunc, WriteLineAction writeLineAction, IWin32Registry registry)
+        {
+            Param.VerifyNotNull(readLineFunc, nameof(readLineFunc));
+            Param.VerifyNotNull(writeLineAction, nameof(writeLineAction));
+            Param.VerifyNotNull(registry, nameof(registry));
+
+            var commandExecutor = new CommandExecutor(_logger, registry);
+
+            // This is the loop that waits for an incoming request (ReadLine), executes a command, and then returns a
+            // response via WriteLine.
+            bool shutdownRequested = false;
+
+            while (!shutdownRequested)
+            {
+                _logger.LogDebug("Waiting for request");
+                string serializedCommand = readLineFunc();
+                _logger.LogInfo("Request received: ", serializedCommand);
+
+                IServiceCommandResponse response;
+
+                if (!ServiceCommand.TryDeserializeFromJsonString(
+                    serializedCommand,
+                    out IServiceCommand command,
+                    out IServiceCommandResponse errorResponse))
+                {
+                    response = errorResponse;
+                }
+                else
+                {
+                    _logger.LogDebug("Executing command: ", command.ToDebugString());
+                    shutdownRequested = command.CommandName == ServiceCommandName.ShutdownServer;
+                    response = commandExecutor.Execute(command);
+                }
+
+                string responseMessage = response.SerializeToJsonString();
+                _logger.LogInfo("Sending response: ", response.ToDebugString());
+                writeLineAction(responseMessage);
+            }
+        }
+    }
+}
---
+++ src/ElevatedDesktopServicesAppTests/ConsoleStreamServerTests.cs
@@ -0,0 +1,110 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleStreamServerTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.ElevatedDesktopServicesAppTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using ElevatedDesktopServicesApp;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using ServiceContracts.CommandBridge;
+    using ServiceContracts.Commands;
+    using Shared.Commands;
+    using Shared.Logging;
+    using SharedWin32Tests.Fakes;
+
+    public class ConsoleStreamServerTests
+    {
+        [Test]
+        public void Ctor_should_throw_on_null_params()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Action action = () => new ConsoleStreamServer(null);
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("logger");
+        }
+
+        //// ===========================================================================================================
+        //// Start Tests
+        //// ===========================================================================================================
+
+        private sealed class FakeClient
+        {
+            private readonly Queue<IServiceCommand> _commands;
+            private readonly List<string> _responses = new List<string>();
+
+            public FakeClient(params IServiceCommand[] commands)
+            {
+                _commands = new Queue<IServiceCommand>(commands);
+            }
+
+            public string LastResponse => _responses.Last();
+            public IReadOnlyList<string> Responses => _responses.AsReadOnly();
+
+            public string SendCommand()
+            {
+                IServiceCommand command = _commands.Count > 0 ? _commands.Dequeue() : new ShutdownServerCommand();
+                string serializedCommand = command.SerializeToJsonString();
+                return serializedCommand;
+            }
+
+            public void ReceiveResponse(string response)
+            {
+                _responses.Add(response);
+            }
+        }
+
+        [Test]
+        public void Start_should_throw_on_null_params()
+        {
+            var server = new ConsoleStreamServer(new NullLogger());
+            Action action = () => server.Start(null, s => { }, new FakeRegistry());
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("readLineFunc");
+
+            action = () => server.Start(() => "", null, new FakeRegistry());
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("writeLineAction");
+
+            action = () => server.Start(() => "", s => { }, null);
+            action.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("registry");
+        }
+
+        [Test]
+        public void Start_should_accept_a_shutdown_request()
+        {
+            var client = new FakeClient();
+            var server = new ConsoleStreamServer(new NullLogger());
+            server.Start(client.SendCommand, client.ReceiveResponse, new FakeRegistry());
+
+            client.LastResponse.Should()
+                .Be("{\"CommandName\":\"ShutdownServer\",\"CommandResult\":true,\"ErrorCode\":\"Success\"}");
+        }
+
+        [Test]
+        public void Start_should_accept_echo_request()
+        {
+            var client = new FakeClient(new EchoCommand("Hello!"));
+            var server = new ConsoleStreamServer(new NullLogger());
+            server.Start(client.SendCommand, client.ReceiveResponse, new FakeRegistry());
+
+            client.Responses.First().Should()
+                .Be("{\"CommandName\":\"Echo\",\"CommandResult\":\"Hello!\",\"ErrorCode\":\"Success\"}");
+        }
+
+        [Test]
+        public void Start_should_accept_a_registry_read_request()
+        {
+            var client = new FakeClient(
+                new RegistryReadIntValueCommand(RegistryBaseKey.CurrentUser, "SubKey", "IntValue", -1));
+            var server = new ConsoleStreamServer(new NullLogger());
+            server.Start(client.SendCommand, client.ReceiveResponse, new FakeRegistry(@"HKCU\SubKey\IntValue=123"));
+
+            client.Responses.First().Should()
+                .Be("{\"CommandName\":\"RegistryReadIntValue\",\"CommandResult\":123,\"ErrorCode\":\"Success\"}");
+        }
+    }
+}
