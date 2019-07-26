a614b762e2c755532ddc125929c196f42905bda0
Justin Rockwood <justinrockwood@hotmail.com>
2019-07-25 17:55:51 -0700

    Add IWin32FileSystem and default implementation

---
+++ src/SharedWin32/CommandExecutors/IO/IWin32FileSystem.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWin32FileSystem.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.SharedWin32.CommandExecutors.IO
+{
+    /// <summary>
+    /// Service contract for performing file system operations. Extracted to an an interface for easy unit testing.
+    /// </summary>
+    public interface IWin32FileSystem
+    {
+        void CopyFile(string sourceFileName, string destinationFileName, bool overwrite);
+    }
+}
---
+++ src/SharedWin32/CommandExecutors/IO/Win32FileSystem.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Win32FileSystem.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace WindowsSettingsClone.SharedWin32.CommandExecutors.IO
+{
+    using System.IO;
+
+    internal class Win32FileSystem : IWin32FileSystem
+    {
+        public void CopyFile(string sourceFileName, string destinationFileName, bool overwrite)
+        {
+            File.Copy(sourceFileName, destinationFileName, overwrite);
+        }
+    }
+}
